b5773af4751c83c950c9daccfe52f00f711ebfbc
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-11-27 10:24:23 +0100

    Finalizamos la clase tareasManager.js

--- scripts/tareasManager.js
+++ scripts/tareasManager.js
@@ -42,4 +42,38 @@ export class TareasManager{
         this.arregloTareas = this.arregloTareas.filter((t) => t.id != idTarea);
         this.setArregloTareas();
     }
+
+    limpiarTodo(){
+        this.arregloTareas = [];
+        this.contador = 0;
+        this.setArregloTareas();
+        this.setContador();
+    }
+
+    getContador(){
+        const cont = localStorage.getItem("contador");
+        return cont;
+    }
+
+    setContador(){
+        localStorage.setItem("contador", this.contador);
+    }
+
+    inicializarContador(){
+        if(this.getContador() != null){
+            this.contador = this.getContador();
+        }
+    }
+
+    getArregloTareas(){
+        this.setContador();
+        const arreglo = JSON.parse(localStorage.getItem("arregloTareas"));
+        return arreglo || [];
+    }
+
+    setArregloTareas(){
+        localStorage.setItem("arregloTareas", JSON.stringify(this.arregloTareas));
+        this.listarTareas();
+    }
+
 }
